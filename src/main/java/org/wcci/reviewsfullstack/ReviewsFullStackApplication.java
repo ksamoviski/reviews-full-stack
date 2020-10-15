b9d37cb77e229f6c061bf1547f88a8551c66c9cb
Karin <samoviskik@gmail.com>
2020-10-15 15:23:59 -0400

    first commit

---
+++ src/main/java/org/wcci/reviewsfullstack/ReviewsFullStackApplication.java
@@ -0,0 +1,13 @@
+package org.wcci.reviewsfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewsFullStackApplication.class, args);
+    }
+
+}
